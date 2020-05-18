ededda50cf407d3c0ed83e77ea860b3ddaa71cf6
RubeRuby <rubenvanderknaap@live.nl>
2020-05-18 09:38:31 +0200

    Revert 2 small changes

--- mastermind/game.py
+++ mastermind/game.py
@@ -60,7 +60,6 @@ class Game:
 
     def fetch_turns(self, game_id):
         get_turns = f"SELECT FROM turns WHERE game_id = {game_id} ORDER BY turn_no asc"
-        
         turns = None
         with DataBase() as db:
             turns = db.read(get_turns)
@@ -71,7 +70,7 @@ class Game:
 
     def do_turn(self):
         turn = Turn()
-        outcome = turn.do_turn() 
+        outcome = turn.do_turn()
         return outcome
         # kan je niet 'return Turn().doTurn()' doen? Al gebruik je deze functie volgens mij niet
 
